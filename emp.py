12715046c9aab0545988f4b936012a87d547f27e
Sarvesh Shelke <sarveshshelke01@gmail.com>
2024-08-28 15:48:01 +0530

    [Sarvesh] Add: Python Script to calculate part time wage of employee

--- emp.py
+++ emp.py
@@ -3,13 +3,13 @@ import random
 def check_attend():
     """
     Description:
-    This fuction makes random choice from 0 or 1
+    This fuction makes random choice from 0 or 1 or 2
     Parameter:
     None
     Return:
-    choice : random choice from 0 or 1
+    choice : random choice from 0 or 1 or 2
     """
-    choice = random.randint(0, 1)
+    choice = random.randint(0, 2)
     return choice
 
 def check_daily_wage(daily_hr, wage_per_hr):
@@ -26,13 +26,29 @@ def check_daily_wage(daily_hr, wage_per_hr):
     print(f"Employee is present full day.")
     print(f"The daily wage is: {daily_wage}")
     
+def check_part_time_wage(partime_hr, wage_per_hr):
+    """
+    Description:
+    This fuction calculates part time wage using formula
+    Parameter:
+    daily_hr: part time hours of work
+    wage_per_hr: wage assined per hour
+    Return:
+    None
+    """
+    daily_half_wage = partime_hr * wage_per_hr
+    print(f"Employee is present for half time.") 
+    print(f"Part time wage is: {daily_half_wage}")
+
 def main():
     daily_hr = 8
     wage_per_hr = 20
+    part_time_hr = 4
     choice=check_attend()
-    if(choice==1):
+    if(choice == 1):
         check_daily_wage(daily_hr, wage_per_hr)
-
+    elif(choice == 2):
+        check_part_time_wage(part_time_hr, wage_per_hr)
     else:
         daily_wage = 0
         print("Employee is Absent...")
