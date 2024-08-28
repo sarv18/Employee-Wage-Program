516ccf8d2cbeb049e81407eebc15ff8fc2edb69e
Sarvesh Shelke <sarveshshelke01@gmail.com>
2024-08-28 15:22:39 +0530

    Sarvesh Add: Python Script to calculate daily wage of an employee

--- emp.py
+++ emp.py
@@ -12,14 +12,31 @@ def check_attend():
     choice = random.randint(0, 1)
     return choice
 
+def check_daily_wage(daily_hr, wage_per_hr):
+    """
+    Description:
+    This fuction calculates daily wage using formula
+    Parameter:
+    daily_hr: hours of work
+    wage_per_hr: wage assined per hour
+    Return:
+    None
+    """
+    daily_wage = daily_hr * wage_per_hr
+    print(f"Employee is present full day.")
+    print(f"The daily wage is: {daily_wage}")
+    
 def main():
-
+    daily_hr = 8
+    wage_per_hr = 20
     choice=check_attend()
     if(choice==1):
-        print("Employee is Present.. ")
+        check_daily_wage(daily_hr, wage_per_hr)
 
     else:
+        daily_wage = 0
         print("Employee is Absent...")
+        print(f"The daily wage is: {daily_wage}")
 
 if __name__=='__main__':
     main()
