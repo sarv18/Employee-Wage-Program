fcaeb2400c9729d4b9bb205d2629eae38f260d5d
Sarvesh Shelke <sarveshshelke01@gmail.com>
2024-08-28 15:00:45 +0530

    [Sarvesh] Add: Python script to check the attendence of employee

--- emp.py
+++ emp.py
@@ -1,1 +1,25 @@
-print("Welcome to Employee wage computation.")
+import random
+
+def check_attend():
+    """
+    Description:
+    This fuction makes random choice from 0 or 1
+    Parameter:
+    None
+    Return:
+    choice : random choice from 0 or 1
+    """
+    choice = random.randint(0, 1)
+    return choice
+
+def main():
+
+    choice=check_attend()
+    if(choice==1):
+        print("Employee is Present.. ")
+
+    else:
+        print("Employee is Absent...")
+
+if __name__=='__main__':
+    main()
